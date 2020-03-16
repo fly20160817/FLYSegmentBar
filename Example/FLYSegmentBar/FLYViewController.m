409fddf792517166f0ced9d1c39805058f903e6b
zhoufei <809176334@qq.com>
2020-03-16 14:27:30 +0800

    删除了vc

--- Example/FLYSegmentBar/FLYViewController.m
+++ Example/FLYSegmentBar/FLYViewController.m
@@ -9,9 +9,10 @@
 #import "FLYViewController.h"
 #import "FLYSegmentBar.h"
 
-@interface FLYViewController () < FLYSegmentBarDelegate >
+@interface FLYViewController () < UIScrollViewDelegate, FLYSegmentBarDelegate >
 
 @property (nonatomic, strong) FLYSegmentBar * segmentBar;
+@property (nonatomic, strong) UIScrollView * scrollView;
 
 @end
 
@@ -22,8 +23,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    
-    self.navigationItem.titleView = self.segmentBar;
+    [self initUI];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,11 +34,70 @@
 
 
 
+#pragma mark - UI
+
+- (void)initUI
+{
+    self.navigationItem.titleView = self.segmentBar;
+    [self.view addSubview:self.scrollView];
+       
+       
+    
+    UIViewController * vc1 = [[UIViewController alloc] init];
+    vc1.view.backgroundColor = [UIColor purpleColor];
+    UIViewController * vc2 = [[UIViewController alloc] init];
+    vc2.view.backgroundColor = [UIColor orangeColor];
+    UIViewController * vc3 = [[UIViewController alloc] init];
+    vc3.view.backgroundColor = [UIColor cyanColor];
+    UIViewController * vc4 = [[UIViewController alloc] init];
+    vc4.view.backgroundColor = [UIColor redColor];
+    NSArray * array = @[vc1, vc2, vc3, vc4];
+    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width * array.count, 0);
+    for ( UIViewController *vc in array )
+    {
+        [self addChildViewController:vc];
+    }
+    
+    
+    //展示第一个页面
+    self.segmentBar.selectIndex = 0;
+}
+
+
+
 #pragma mark - FLYSegmentBarDelegate
 
 -(void)segmentBar:(FLYSegmentBar *)segmentBar didSelectIndex:(NSInteger)toIndex fromIndex:(NSInteger)fromIndex
 {
-    NSLog(@"%ld -> %ld", (long)fromIndex, (long)toIndex);
+    [self showChildVCViewsAtIndex:toIndex];
+}
+
+
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    NSInteger idx = scrollView.contentOffset.x / scrollView.frame.size.width;
+    self.segmentBar.selectIndex = idx;
+}
+
+
+
+#pragma mark - private methods
+
+- (void)showChildVCViewsAtIndex: (NSInteger)index
+{
+    // 数据过滤
+    if ( index < 0 || index >= self.childViewControllers.count )
+    {
+        return;
+    }
+    
+    UIViewController * vc = self.childViewControllers[index];
+    vc.view.frame = CGRectMake(index * self.scrollView.frame.size.width, 0, self.scrollView.frame.size.width, self.scrollView.frame.size.height);
+    [self.scrollView addSubview:vc.view];
+    [self.scrollView setContentOffset:CGPointMake(self.scrollView.frame.size.width * index, 0) animated:YES];
 }
 
 
@@ -76,6 +135,19 @@
 
     }
     return _segmentBar;
+}
+
+-(UIScrollView *)scrollView
+{
+    if ( _scrollView == nil )
+    {
+        _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+        _scrollView.delegate = self;
+        _scrollView.showsHorizontalScrollIndicator = NO;
+        _scrollView.pagingEnabled = YES;
+        _scrollView.bounces = NO;
+    }
+    return _scrollView;
 }
 
 @end
--- Example/Pods/Target Support Files/FLYSegmentBar/FLYSegmentBar-umbrella.h
+++ Example/Pods/Target Support Files/FLYSegmentBar/FLYSegmentBar-umbrella.h
@@ -12,7 +12,6 @@
 
 #import "FLYSegmentBar.h"
 #import "FLYSegmentBarConfig.h"
-#import "FLYSegmentBarVC.h"
 #import "UIView+FLYSegmentBar.h"
 
 FOUNDATION_EXPORT double FLYSegmentBarVersionNumber;
